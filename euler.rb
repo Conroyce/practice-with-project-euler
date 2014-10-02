e811e5afb64925dd7c2882db95eb1e29f3e060f0
Conroyce <CK@Connors-MacBook-Air.local>
2014-10-02 13:50:05 -0700

    added 2 more problems

--- euler.rb
+++ euler.rb
@@ -10,4 +10,70 @@ def calc_sum
 	arr.each { |n| sum += n }
 	return sum	
 end
-print calc_sum
+puts "Multiples of 3 and 5: #{calc_sum}"
+
+
+#sum square difference
+def sum_square
+	sumsquare = 0
+	(1..100).each do |n|
+		sumsquare += n**2
+	end
+	addsquare = 0
+	sum = 0
+	(1..100).each do |n|
+		sum += n
+	end
+	addsquare = sum**2
+	total = addsquare - sumsquare		
+		
+end	
+puts "sum square differnce: #{sum_square}"
+
+
+#Even Fibonacci Numbers
+def fibonacci
+	ans = []
+	arr = [1,2]
+	sum = 0
+	(2...4000000).each do |n|
+	arr.push(arr[n-1] + arr[n-2])
+	end
+
+	arr.each do |n|
+		if n%2 == 0
+			ans.push(n)
+		end	
+	end	
+	ans.each do |n|
+		sum += n
+	end
+	return sum	
+end	
+
+print fibonacci
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
